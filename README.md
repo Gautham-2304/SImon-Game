301ddbabfafc1c16dfac91fb9043c9a447069f0e
Esarapu Gautham <gautham0423@gmail.com>
2026-06-15 20:21:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# SImon-Game
-An interactive Simon memory game that generates random color sequences for players to repeat, featuring sound effects, animations, and level progression.
+# Simon Game
+
+A browser-based implementation of the classic Simon memory game built using HTML, CSS, JavaScript, and jQuery.
+
+## How to Play
+
+1. Press any key to start the game.
+2. Watch the flashing color sequence.
+3. Repeat the sequence by clicking the colored buttons.
+4. Each level adds a new color to the sequence.
+5. Clicking the wrong button ends the game.
+
+## Technologies Used
+
+* HTML5
+* CSS3
+* JavaScript
+* jQuery
+
+## Features
+
+* Random sequence generation
+* Sound effects
+* Button animations
+* Level progression
+* Game over detection
+
+## Project Structure
+
+```text
+index.html
+styles.css
+game.js
+sounds/
+```
+
+## Author
+
+Gautham
---
+++ game.js
@@ -0,0 +1,76 @@
+var gamePattern = [];
+var userClickedPattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var level = 0;
+var started = false;
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
+    
+}
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+})
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+$(document).keypress(function() {
+    if (!started) {
+        nextSequence();
+        started = true;
+    }
+})
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        if (userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+            $("h1").text("Game Over, press any key to restart");
+        }, 200);
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
